f954e26509a3c9f8aeb34cd38dbf0b99a354598a
shubha-rajan <shubhadayini@google.com>
2019-12-16 14:58:31 -0800

    change background color of messages based on sentiment

--- frontend/src/app/message-display/message-display.component.ts
+++ frontend/src/app/message-display/message-display.component.ts
@@ -9,4 +9,18 @@ import { Component,  Input  } from '@angular/core';
 })
 export class MessageDisplayComponent {
   @Input() messages: Array<Object>;
+
+  getSentimentClass(message){
+    if (!message.sentiment) {
+      return "";
+    }
+    if (message.sentiment.score > 0.25){
+      return "positive";
+    } else if (message.sentiment.score <= 0.25 && message.sentiment.score >= -0.25) {
+      return "neutral";
+    } else if (message.sentiment.score < -0.25) {
+      return "negative";
+    } 
+  }
+  
 }
